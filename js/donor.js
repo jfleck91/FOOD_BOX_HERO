2418e37439413ba825ac9cbaacf1c5b2bfb22bcb
jfleck91 <jfleck91@gmail.com>
2018-05-16 00:55:02 -0700

    donor form added

---
+++ js/donor.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.timepicker').timepicker();
+  });
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+          
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
