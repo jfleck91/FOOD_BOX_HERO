548ed24749ce62deb9e42e74d0027c11dd5a5f24
jfleck91 <jfleck91@gmail.com>
2018-05-17 21:51:25 -0700

    back to where we started sort of

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -3,5 +3,37 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 
+var app = express();
 
-module.exports = router;
+// Root get route
+module.exports = function(app) {
+
+app.get("/", function(req, res) {
+    connection.query("SELECT * FROM donations;", function(err, data) {
+      if (err) throw err;
+      res.render("home",  {donations : data});
+    });
+  });
+  
+  app.get("/donor", function(req, res) {
+    connection.query("SELECT * FROM donations;", function(err, data) {
+      if (err) throw err;
+      res.render("donations",  {donations : data});
+    });
+  });
+  
+  app.get("/recipient", function(req, res) {
+    connection.query("SELECT * FROM donations;", function(err, data) {
+      if (err) throw err;
+      res.render("recipients",  {donations : data});
+    });
+  });
+  
+  app.get("/login", function(req, res) {
+    connection.query("SELECT * FROM donations;", function(err, data) {
+      if (err) throw err;
+      res.render("login",  {donations : data});
+    });
+  });
+
+};
