5aa287165cb36cbb714fd1f8ec47e5d2f646a5c8
benyamerican <35763971+benyamerican@users.noreply.github.com>
2018-05-19 13:16:56 -0700

    recipients table created

--- models/recipients.js
+++ models/recipients.js
@@ -4,99 +4,63 @@
 module.exports = function(sequelize, DataTypes) {
 
     // Creates a "donations" model that matches up with DB
-    var Recipients = sequelize.define("donations", {
+    var Recipients = sequelize.define("recipients", {
     
-        business: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            len: [1]
-        }
-      },
-    
-      food: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-      }
-    },
-    
-    
-    
-        quantity_avalaible: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          validate: {
-          len: [1]
-        }
-      },
-            
-        address: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-          len: [1]
-        }
-      },
-            
-        lastCall: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-          len: [1]
-        }
-      },
-    
-        pickupDate: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-          len: [1]
-        }
-      },
-    
-        donorText: {
-          type: DataTypes.TEXT,
-          allowNull: true,
-          validate: {
-          len: [1]
-        }
-      },
-                  
-        created_at: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          validate: {
-          len: [1]
-          }
-        },
-        updated_at: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          validate: {
-          len: [1]
-        }
-      },      
-    
-     category:{
-       type:DataTypes.STRING,
-       allowNull: false,
-       validate: {
-        len: [1]
-      }
-     },
-    
-     allergen:{
-      type:DataTypes.STRING,
-      allowNull: false,
-      validate: {
-       len: [1]
-     }
-    },
+category: {
+type: DataTypes.STRING,
+allowNull: false,
+validate: {
+len: [1]
+}
+},  
+
+quantity_needed: {
+type: DataTypes.INTEGER,
+allowNull: false,
+validate: {
+len: [1]
+}
+},
+      
+recipientText: {
+type: DataTypes.TEXT,
+allowNull: true,
+validate: {
+len: [1]
+}
+},
+               
+name:{
+type:DataTypes.STRING,
+allowNull: false,
+validate: {
+len: [1]
+}
+},
+contact:{
+  type:DataTypes.STRING,
+  allowNull: false,
+  validate: {
+  len: [1]
+}
+},
+created_at: {
+type: DataTypes.DATE,
+allowNull: false,
+validate: {
+len: [1]
+}
+},
+updated_at: {
+type: DataTypes.DATE,
+allowNull: false,
+validate: {
+len: [1]
+}
+},      
     
       
-     });
+  });
     
     // Syncs with DB
     return Recipients;
