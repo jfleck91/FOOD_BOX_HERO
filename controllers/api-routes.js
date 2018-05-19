60e23f82c8b2cfc883d3d42a8f4a8996d78da671
benyamerican <35763971+benyamerican@users.noreply.github.com>
2018-05-19 11:10:32 -0700

    Connection MY way ........BITCHES

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -43,6 +43,8 @@ module.exports = function(app) {
 
       address: donate.address,
 
+      
+
       category: donate.category,
 
       // food type dropdown select
--- server.js
+++ server.js
@@ -1,56 +1,38 @@
-
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
 
+// Sets up the Express App
+// =============================================================
 var app = express();
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
-
-
- //Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "George91",
-  database: "foodBoxHero_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-});
-
-//Allows us to use local styling from our public/assets folder.
-app.use(express.static('public'));
 
 // Requiring our models for syncing
 var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
 
 // Static directory
 app.use(express.static("public"));
 
 // Routes
 // =============================================================
-require("./controllers/api-routes.js")(app);
 require("./controllers/html-routes.js")(app);
+require("./controllers/api-routes.js")(app);
+// require("./controllers/post-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
