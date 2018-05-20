a11814275224771dfdb7ff8d1e23b0499902f907
benyamerican <35763971+benyamerican@users.noreply.github.com>
2018-05-19 17:17:33 -0700

    POST and GET routes both work like a swiss watch

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,55 +1,59 @@
 // *********************************************************************************
 // api-routes.js - this file offers a set of routes for displaying and saving data to the db
 // *********************************************************************************
-
 // Dependencies
-var db = require("../models");
-var express = require("express");
-var mysql = require("mysql");
-var app = express();
 // =============================================================
 
-// Routes
-// =============================================================
+var express = require("express");
+var router = express.Router();
+//var path = require("path");
+//var mysql = require("mysql");
+var app = express();
+
+var db = require("../models");
+
+// Root get route
 module.exports = function(app) {
 
-  // Get all donations
-  app.get("/donations", function(req, res) {
-    db.donations.findAll({}).then(function(results) {
-      // results are available to us inside the .then
-      res.json(results);
-    });
+ // Routes
+// =============================================================
+ // Get all chirps
+ app.get("/api/all", function(req, res) {
+   // Finding all DOnations, and then returning them to the user as JSON.
+   // Sequelize queries are asynchronous, which helps with perceived speed.
+   // If we want something to be guaranteed to happen after the query, we'll use
+   // the .then function
+   db.donations.findAll({}).then(function(results) {
+     // results are available to us inside the .then
+     res.json(results);
+   });
+ });
+ // Add a donation/api/newdonation
+  app.get("/api/adddonations", function(req, res) {
+    var d = req.body;
+    console.log("Donation:");
+    console.log(d);
+    db.donations.create({
+    
+      business:d.business,
+      food:d.food,
+      quantity_avalaible:d.quantity_avalaible,
+      address:d.address,
+      lastCall:d.lastCall,
+      pickupDate:d.lastCall,
+      donorText:d.donorText,
+      created_at:d.created_at,
+      updated_at:d.updated_at,
+      category:d.category,
+      allergen:d.allergen
 
-  });
-
-  // Add a donation
-  app.post("/api/newdonations", function(req, res) {
-
-      // Take the request...
-      var donate = req.body;
-
-    console.log("Donations Data:");
-    console.log(donate);
-
-   
-    Donations.create({
-      business: donate.business,
-      donorText: donate.donorText,
-      quantity_avalaible: donate.quantity_avalaible,
-      category: donation.category,
-      address: donate.address,
-      category: donate.category,
-      // food type dropdown select
-      // allergens checkbox select
-      pickupDate: donate.pickupDate,
-      lastCall: donate.lastCall,
-      created_at: donate.created_at,
       
+      ///////
     }).then(function(results) {
-      // `results` here would be the newly created donations
+      // `results` here would be the newly created Donor
       res.end();
     });
-
   });
+ };
 
-};
+
