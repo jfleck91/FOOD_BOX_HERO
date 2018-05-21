98a1bff992fb981cf318d690608d86832fd1b590
unknown <benyamerican@yahoo.com>
2018-05-21 13:12:29 -0700

    changes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var session    = require('express-session')
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8000;
 
 
 // For Passport
