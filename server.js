55c45f2ded37a8002d4831b2e7fe47a23fc0ff08
jfleck91 <jfleck91@gmail.com>
2018-05-20 18:42:53 -0700

    updates

--- server.js
+++ server.js
@@ -4,14 +4,30 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
+var env = require('dotenv').load();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var passport   = require('passport')
+var session    = require('express-session')
+
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+
+// For Passport
+ 
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true}));
+ // session secret
+ 
+app.use(passport.initialize());
+ 
+app.use(passport.session()); 
+
+// persistent login sessions
 
 // Requiring our models for syncing
 var db = require("./models");
