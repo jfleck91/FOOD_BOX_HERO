627e3a9c1a94fdf487410965d0f22337c509abcf
jfleck91 <jfleck91@gmail.com>
2018-05-21 11:09:01 -0700

    seperate signup page added

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -25,10 +25,13 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
       res.render("login");
     });
-  };
-
   
 
+  app.get("/signup", function(req, res) {
+    res.render("signup");
+  });
+
+};
 
   
 
